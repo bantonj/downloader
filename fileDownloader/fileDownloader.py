bf46f15715019e3d92c575b80623ac39764af1e3
Joshua Banton <bantonj@gmail.com>
2013-11-15 17:21:01 -0500

    Added S3 and rate limit support.

--- fileDownloader/fileDownloader.py
+++ fileDownloader/fileDownloader.py
@@ -8,7 +8,11 @@ import urlparse
 import urllib
 import sys
 import socket
-import fd_s3
+try:
+    import fd_s3
+except ImportError:
+    print 'S3 downloading disabled, missing s3.py'
+from time import time, sleep
  
 version = "0.4.0"
 
@@ -24,6 +28,10 @@ class DownloadFile(object):
     If a non-standard port is needed just include it in the url (http://example.com:7632).
     
     #NOTE: S3 requires s3.amazonaws.com/hanstest/s3.py.
+    
+    #Rate Limiting:
+        rate_limit = the average download rate in Bps
+        rate_burst = the largest allowable burst in bytes
     
     Basic usage:
         Simple
@@ -41,8 +49,9 @@ class DownloadFile(object):
     """        
     
     def __init__(self, url=None, localFileName=None, auth=None, timeout=120.0, autoretry=False, retries=5,
-                 fast_start=False, aws_key=None, aws_secret_key=None, aws_bucket_name=None, aws_file_key=None):
-        """Note that auth argument expects a tuple, ('username','password')"""
+                 fast_start=False, aws_key=None, aws_secret_key=None, aws_bucket_name=None, aws_file_key=None,
+                 rate_limit_on=False, rate_limit=500, rate_burst=1000):
+        """Note that auth argument expects a tuple, ('username','password')."""
         self.url = url
         self.urlFileName = None
         self.progress = 0
@@ -71,12 +80,22 @@ class DownloadFile(object):
             self.urlFilesize = None
         if not self.localFileName: #if no filename given pulls filename from the url
             self.localFileName = self.getUrlFilename(self.url)
+        self.rate_limit_on = rate_limit_on
+        self.rate_limit = rate_limit
+        self.rate_burst = rate_burst
+        if self.rate_limit_on:
+            self.bucket = TokenBucket(self.rate_burst, self.rate_limit)
+            
         
     def __downloadFile__(self, urlObj, fileObj, callBack=None):
         """starts the download loop"""
         if not self.fast_start:
             self.fileSize = self.getUrlFileSize()
         while 1:
+            if self.rate_limit_on:
+                if not self.bucket.spend(8192):
+                    sleep(1)
+                    continue
             try:
                 data = urlObj.read(8192)
             except (socket.timeout, socket.error) as t:
@@ -302,3 +321,33 @@ class DownloadFile(object):
 class FileDownloaderError(Exception):
     def __init(self, message=''):
         self.message = message
+        
+
+class TokenBucket(object):
+    """An implementation of the token bucket algorithm.
+       Slightly modified from http://code.activestate.com/recipes/511490-implementation-of-the-token-bucket-algorithm/"""
+    
+    def __init__(self, bucket_size, fill_rate):
+        """tokens is the total tokens in the bucket. fill_rate is the
+        rate in tokens/second that the bucket will be refilled."""
+        self.capacity = float(bucket_size)
+        self._tokens = float(bucket_size)
+        self.fill_rate = float(fill_rate)
+        self.timestamp = time()
+
+    def spend(self, tokens):
+        """Spend tokens from the bucket. Returns True if there were
+        sufficient tokens otherwise False."""
+        if tokens <= self.get_tokens():
+            self._tokens -= tokens
+        else:
+            return False
+        return True
+
+    def get_tokens(self):
+        now = time()
+        if self._tokens < self.capacity:
+            delta = self.fill_rate * (now - self.timestamp)
+            self._tokens = min(self.capacity, self._tokens + delta)
+        self.timestamp = now
+        return self._tokens
